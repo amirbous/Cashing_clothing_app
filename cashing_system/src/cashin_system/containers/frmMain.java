1b13df251d5fb1515cd3c53357e17b5962a30a3b
amirbous <86443415+amirbous@users.noreply.github.com>
2021-07-01 21:58:57 +0100

    Add files via upload

--- cashing_system/src/cashin_system/containers/frmMain.java
+++ cashing_system/src/cashin_system/containers/frmMain.java
@@ -3,13 +3,18 @@ package cashin_system.containers;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 
+import cashing_system.processing.ActionHandler;
+
 public class frmMain extends JFrame{
 		
+		
+	
 		JButton disconnect = new JButton("end session");
 		public frmMain() {
+			
 			this.setSize(400, 400);
 			this.setVisible(true);
-			this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+			
 			this.add(disconnect);
 			
 		}
